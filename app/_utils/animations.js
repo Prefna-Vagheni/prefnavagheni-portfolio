f1b65670bc18bd810be166a1c2124031095a6949
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-01-05 21:33:57 +0300

    Add: Fade in animations

---
+++ app/_utils/animations.js
@@ -0,0 +1,25 @@
+// utils/animations.js
+export const transitions = {
+  duration: 0.8,
+  ease: [0.22, 1, 0.36, 1], // Custom "luxury" quint ease
+};
+
+export const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.2,
+    },
+  },
+};
+
+export const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: transitions,
+  },
+};
