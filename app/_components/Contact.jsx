2998ec3ad9a3f144a0128c5c50413cec340536a5
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-01-06 22:16:36 +0300

    Polish Component

--- app/_components/Contact.jsx
+++ app/_components/Contact.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import {} from 'framer-motion';
 import { Terminal, Send, Github, Linkedin, Twitter } from 'lucide-react';
 
 export default function Contact() {
