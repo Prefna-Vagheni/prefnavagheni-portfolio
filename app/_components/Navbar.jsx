0af72f650fad5c0fafbcff6ff6d3615eca998b71
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-01-07 19:33:26 +0300

    Polish Component

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const [mounted, setMounted] = useState(false);
 
   // Avoid hydration mismatch
-  useEffect(() => setMounted(true), []); //ESLint ignore-l
+  useEffect(() => setMounted(true), []); //ESLint ignore-
 
   const navItems = [
     { name: 'Engine', href: '#hero' },
