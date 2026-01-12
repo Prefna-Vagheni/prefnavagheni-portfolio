2f639026657b96747a4d11f2e1f93fcb603ab6d6
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-01-12 22:40:37 +0300

    Polish Component

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -30,7 +30,7 @@ export default function Navbar() {
           <Command size={20} />
         </div>
 
-        {/* Navigati*/}
+        {/* Naviga*/}
         <div className="hidden md:flex items-center gap-1 px-4">
           {navItems.map((item) => (
             <a
