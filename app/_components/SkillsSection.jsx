6afba92b9fe154981a5f2db9ed820db667ad6a03
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-02-03 22:21:36 +0000

    Polish component

--- app/_components/SkillsSection.jsx
+++ app/_components/SkillsSection.jsx
@@ -297,7 +297,7 @@ export default function SkillsSection() {
           </div>
         </motion.div>
 
-        {/* Currently  */}
+        {/* Currently */}
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
