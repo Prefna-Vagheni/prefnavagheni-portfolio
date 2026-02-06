90c16f6f77e617c6e2a6dcecc7b57fdb700fe2ef
Prefna Vagheni <prefnavagheni54@gmail.com>
2026-02-06 23:24:53 +0300

    Polish component

--- app/_components/TechnicalSkills.jsx
+++ app/_components/TechnicalSkills.jsx
@@ -50,7 +50,7 @@ function TechnicalSkills() {
     },
   };
 
-  // Helper function to get proficiency
+  // Helper function to get proficien
   const getProficiencyColor = (level) => {
     if (level >= 90) return 'bg-gray-500';
     if (level >= 75) return 'bg-gray-500/80';
